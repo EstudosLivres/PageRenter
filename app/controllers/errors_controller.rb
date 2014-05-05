2b00c426b27641ae3407e77cb5ce5201229b062f
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-05 18:19:28 -0300

    idiom threat removed from the error threater

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,9 +3,4 @@ class ErrorsController < ApplicationController
     # raise ActionController::RoutingError.new(params[:path])
     render :file => 'public/404.html.erb'
   end
-
-  # Put an Idiom, using the default browse idiom, to the URL
-  def redirect
-    render :file => 'public/redirect.html.erb'
-  end
 end
