c0eb0b21e0cbeadf72abc7673e71e16eef66768d
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-08 22:29:52 -0300

    Refector the Specs

--- spec/controllers/advertiser_controller_spec.rb
+++ spec/controllers/advertiser_controller_spec.rb
@@ -2,12 +2,10 @@ require 'spec_helper'
 
 describe AdvertiserController do
 
-  describe "GET 'index' with idiom" do
-    it "returns http success with 'en' idiom" do
-      expect(:get => '/en/advertiser').to route_to(:controller => 'advertiser',:idiom => 'en',:action => 'index')
-    end
-    it "returns http success with 'pt' idiom" do
-      expect(:get => '/pt/advertiser').to route_to(:controller => 'advertiser',:idiom => 'pt',:action => 'index')
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
     end
   end
 
