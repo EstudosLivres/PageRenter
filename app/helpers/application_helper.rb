2a897a8a81ae63b7ed78c728a72a0635e7c73a4a
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-09 00:32:48 -0300

    Helper to do not have rails logic on the view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def session_role_redir
 
   end
+
+  def role_name
+    @params_role = params['controller'][0...-1]
+  end
+
+  def role_icon
+    if @params_role == 'publisher' then return 'fa-rocket' else return 'fa-bullhorn' end
+  end
 end
