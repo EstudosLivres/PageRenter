7f1cd9f92389b2d82a1f42450e7fcd788e742faf
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-09 11:12:02 -0300

    refatoração para remover o acoplamento entre os helpers pelo attr da classe e criei uma coplamento por chamdas de métodos (remove a necessidade de verificar se o attr foi setado)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,18 @@ module ApplicationHelper
   end
 
   def role_name
-    @params_role = params['controller'][0...-1]
+    params_role = params['controller'][0...-1]
   end
 
   def role_icon
-    if @params_role == 'publisher' then return 'fa-rocket' else return 'fa-bullhorn' end
+    if role_name == 'publisher' then return 'fa-rocket' else return 'fa-bullhorn' end
+  end
+
+  def role_actions
+    if role_name == 'publisher'
+      return [{ :name => 'ConvidarAmigos', :path => 'publishers/invite' }]
+    else
+      return [{ :name => 'Recomendar', :path => 'advertisers/recommend' }]
+    end
   end
 end
