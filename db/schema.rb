aba51460a222d7c2b1e18dd34e51fd1f4798c12b
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-15 10:03:18 -0300

    Tests Icon per Role

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501153843) do
-
-  create_table "publishers", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -13,16 +13,31 @@ require 'spec_helper'
 describe ApplicationHelper do
 
   describe 'roles' do
-    context "Role Advertiser" do
+    # Advertiser
+    context "Role #Advertiser" do
+      subject! { controller.params = { 'controller' => 'advertisers' } }
+      adv_icon = 'fa-bullhorn'
+
       it "Role should be Advertiser" do
-        controller.params = { 'controller' => 'advertisers' }
         helper.role_name.should == 'advertiser'
       end
+
+      it "Advertiser icon should be #{ adv_icon }" do
+        helper.role_icon.should == adv_icon
+      end
     end
-    context "Role Publisher" do
-      it "Role should be Advertiser" do
-        controller.params = { 'controller' => 'publishers' }
+
+    # Publisher
+    context "Role #Publisher" do
+      subject! { controller.params = { 'controller' => 'publishers' } }
+      pub_icon = 'fa-rocket'
+
+      it "Role should be Publisher" do
         helper.role_name.should == 'publisher'
+      end
+
+      it "Publisher icon should be #{ pub_icon }" do
+        helper.role_icon.should == pub_icon
       end
     end
   end
