7d97e012c16db0c77c3b60813a0cd70e1db2d47b
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-17 02:23:05 -0300

    Respond with Erro JSON if any validate Throws

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,15 @@
 class API::UsersController < API::BaseAPIController
   # Login user By System request (internal dependences)
   def system_signup_signin
+    # Except to eliminate the Attr from the Hash (it is a attr to another Model)
+    json_received = JSON.parse(params['user'])
+    user_hash = json_received.except!('role')
+    user_role = json_received['role']
+    user = User.new(user_hash)
+
+    unless user.save
+     render json:  user.errors.messages.to_json
+    end
 
   end
 
