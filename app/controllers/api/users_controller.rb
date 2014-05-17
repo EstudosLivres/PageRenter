b16bbdc96aa07f182b85ed0bc2d85d1eef500358
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-17 00:00:48 -0300

    Added diference between Third and Nested SignIn_SignUp: Methods to manipulate it

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,12 @@
 class API::UsersController < API::BaseAPIController
+  # Login user By System request (internal dependences)
+  def system_signup_signin
 
-  # Logar o usuário via token
-  def login
+  end
+
+  # Login user By API request (third dependences)
+  def api_signup_signin
     token_manager = API::Concerns::TokenManager.new(params[:email], params[:password], params[:access_token])
     render json: token_manager.token.to_json
   end
-
 end
