1efe89328d1775e7eefd087211a1e049edaeee75
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-17 00:59:46 -0300

    criação das validações dos atributos para persistir o usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   before_save :encrypt_password
 
+  validates :name, presence: true, length: { in: 3..55 }, on: :create
+  validates :nick, presence: true, length: { in: 3..30 }, uniqueness: true, on: :create, on: :update
+  validates :email, presence: true, length: { in: 7..55 }, uniqueness: true, on: :create, on: :update
+  validates :locale, presence: true, length: { is: 5 }, on: :create
+  validates :password, length: { is: 60 }, on: :create
+
   # Encrypt the pasword using BCrypt
   def encrypt_password
     if password.present?
