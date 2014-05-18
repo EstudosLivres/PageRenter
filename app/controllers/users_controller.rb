c0737d694564af804f6d8c4478acc95c4adfed9b
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-17 23:31:44 -0300

    permit foreign post to login and start the login func

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  def sign_up
-  end
+  protect_from_forgery except: :login
 
   def login
+    user_params = params['user']
+    User.authenticate(user_params['email'], user_params['password'])
   end
 
   def sign_out
