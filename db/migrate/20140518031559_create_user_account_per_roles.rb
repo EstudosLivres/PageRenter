22c0dfd7cbd366fa0329526184cc40e0a18443a6
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-18 00:20:39 -0300

    Multi user Table

---
+++ db/migrate/20140518031559_create_user_account_per_roles.rb
@@ -0,0 +1,12 @@
+class CreateUserAccountPerRoles < ActiveRecord::Migration
+  def change
+    create_table :user_account_per_roles do |t|
+      t.string :name, limit: 55, null: true
+      t.boolean :default_role, null: false
+      t.belongs_to :role
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
