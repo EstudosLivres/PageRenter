f3bae9b39ea5a59d8fb9e7d8b94139155826c89c
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-19 00:39:20 -0300

    Associate Page -> Session

---
+++ app/models/page_account.rb
@@ -0,0 +1,4 @@
+class PageAccount < ActiveRecord::Base
+  # Relations
+  has_and_belongs_to_many :social_sessions
+end
--- app/models/social_session.rb
+++ app/models/social_session.rb
@@ -2,4 +2,5 @@ class SocialSession < ActiveRecord::Base
   # Relations
   belongs_to :user
   belongs_to :social_network
+  has_and_belongs_to_many :page_accounts
 end
