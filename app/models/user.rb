1e4c061b571e19d7441bab8adc54722f4ba15dc3
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-19 01:07:23 -0300

    Page added to the hash to persist

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,11 +59,19 @@ class User < ActiveRecord::Base
   # Create an user_hash (instanciable) from a social_hash RETURNS: [:user] & [:social_session]
   def self.create_user_hash_from_social social_hash
     user = social_hash['login']
+    pages = social_hash['pages']['data']
 
     hash_return = {:user => {}, :social_session => {}}
 
     hash_return[:user] = {name: user['name'], nick: user['username'], email: user['email'], locale: user['locale'], 'role' => 'publisher'}
     hash_return[:social_session] = {id_on_social: user['id'], name: user['name'], username: user['username'], email: user['email'], gender: user['gender'], locale: user['locale'], gender: user['gender'], count_friends: user['count_friends'], user_id: user['id'], social_network_id: user['network_id']}
+    hash_return[:pages] = []
+
+    # Append the pages
+    pages.each do |page|
+      page = page[1]
+      hash_return[:pages].append({ id_on_social: page['id'], name: page['name'], category: page['category'], access_token: page['access_token'] })
+    end
 
     hash_return
   end
