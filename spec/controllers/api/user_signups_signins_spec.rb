8a0a079d12c6a18bf266c53d12a57e506672ed94
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-19 17:52:26 -0300

    Added subjects to DRY dev SPecs

--- spec/controllers/api/user_signups_signins_spec.rb
+++ spec/controllers/api/user_signups_signins_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 describe "User Registration & LogIn" do
   subject(:http_helper) { API::Concerns::HttpTestHelper.new }
+  subject(:empty_user) { {'user' => ''} }
+  subject(:valid_user) { {'user' => {role: 'publisher', locale: 'pt', name: 'Ilton Garcia', nick: '', email: '', password: ''}} }
+  subject(:invalid_user) { {'user' => {role: '', locale: '', name: '', nick: '', email: '', password: ''}} }
+
   before(:each) do
     @controller = API::UsersController
   end
 
   # Submit an user empty hash as param for SignUp_SignIn
   describe "SignUp user BY #INVALID user FORM" do
-    subject(:invalid_form_resp) { http_helper.post('/system/signup_signin', {'user' => ''}) }
+    subject(:invalid_form_resp) { http_helper.post('/system/signup_signin', empty_user) }
 
     it "Should be accessible" do invalid_form_resp.code.to_i.should == 200 end
     it "Should has an error" do JSON.parse(invalid_form_resp.body)['status'].should == 'error' end
