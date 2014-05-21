a0537c2fd64314af0c5c79d6bef22c6c5acd59d8
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-21 10:29:27 -0300

    SocialSession validated

--- app/models/social_session.rb
+++ app/models/social_session.rb
@@ -3,4 +3,13 @@ class SocialSession < ActiveRecord::Base
   belongs_to :user
   belongs_to :social_network
   has_and_belongs_to_many :page_accounts
+
+  # Rails validations
+  validates :id_on_social, presence: true, length: { maximum: 45 }
+  validates :name, presence: true, length: { maximum: 65 }
+  validates :username, presence: true, length: { maximum: 45 }
+  validates :email, presence: true, length: { maximum: 55 }
+  validates :gender, presence: false, length: { maximum: 10 }
+  validates :locale, presence: true, length: { maximum: 5 }
+  validates :count_friends, presence: true
 end
