060cd0b35f65c17a6b5c3ad8c83e03b416d31409
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-22 10:10:28 -0300

    Redirect to the user role page, dynamic on request the root

---
+++ app/controllers/redirect_controller.rb
@@ -0,0 +1,13 @@
+class RedirectController < ApplicationController
+  def index
+    loged_user = User.find(session['user_id'])
+
+    if !loged_user.nil?
+      default_role = Profile.get_default_role(loged_user.profiles)
+      redirect_to :controller => "#{default_role}s", :action => 'index'
+    else
+      # Redirect to the land
+      redirect_to 'http://pagerenter.com.br'
+    end
+  end
+end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,10 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   belongs_to :role
 
+  def self.get_default_role profiles
+    profiles.each do |profile|
+      if profile.default_role then return profile.role.name end
+    end
+  end
+
 end
