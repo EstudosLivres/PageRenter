46a4484a8cb38e219d8640c4f2ad83791fdc039b
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-22 23:52:46 -0300

    Added the SPec test for no user session, which means no current profile to be loaded

--- spec/controllers/redirect_controller_spec.rb
+++ spec/controllers/redirect_controller_spec.rb
@@ -20,5 +20,22 @@ describe RedirectController do
         expect(response).to redirect_to('/advertisers')
       end
     end
+
+    context "#Publisher" do
+      subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
+      subject!(:publisher_profile_session) { session['user_id'] = publisher_profile.user.id }
+      it "should responde #/publishers INDEX" do
+        get :index
+        expect(response).to redirect_to('/publishers')
+      end
+    end
+
+    context "#InvalidRole" do
+      subject!(:user) { FactoryGirl.create(:user) }
+      it "should responde #/advertisers INDEX" do
+        get :index
+        expect(response).to redirect_to('http://pagerenter.com.br')
+      end
+    end
   end
 end
