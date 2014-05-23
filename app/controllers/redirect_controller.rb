3c69d039179c418fe5b0564d96c6785b7eee7ae8
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-22 23:50:45 -0300

    ADDED: unless session user_id nil * Prevent broken SQL

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,6 +1,6 @@
 class RedirectController < ApplicationController
   def index
-    loged_user = User.find(session['user_id'])
+    loged_user = User.find(session['user_id']) unless session['user_id'].nil?
 
     if !loged_user.nil?
       default_role = Profile.get_default_role(loged_user.profiles)
