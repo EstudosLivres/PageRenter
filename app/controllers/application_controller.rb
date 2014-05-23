e7a0c1a2acfc6362af82848e843d9cf43ed0997f
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-23 11:31:34 -0300

    Translate based on the user locale on DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    if session['user_idiom'].nil? then session['user_idiom'] = User.find(session['user_id']).locale[0..1] end
+    I18n.locale = session['user_idiom'] || I18n.default_locale
+  end
 
   def self.land_url
     # 'http://pagerenter.com.br'
