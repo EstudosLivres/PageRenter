39a1675fa0b5d84e25832085b9ebc5b688641f9f
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-23 01:12:49 -0300

    test if login error render the landing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 require 'factory_girl_rails'
 
 describe UsersController do
-  context "POST 'login'" do
+  context "POST #VALID 'login'" do
     subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
     it "Redirect to it role url" do
       post :login, { 'user' => { :password => '123', :email => '11@1.1' } }
       expect(response).to redirect_to('/publishers')
+    end
+  end
+
+  context "POST #INVALID 'login'" do
+    subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
+    it "Redirect to it role url" do
+      post :login, { 'user' => { :password => '', :email => '11@1.1' } }
+      expect(response).to redirect_to('http://pagerenter.com.br')
     end
   end
 end
