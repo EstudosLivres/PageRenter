06d5165c4d48bf0a7d2a8c61c2aa9dcdf95719d2
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-23 01:08:16 -0300

    Test the verification of the DefaultRole

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
+require 'factory_girl_rails'
 
 describe UsersController do
-=begin
-  describe "GET 'sign_out'" do
-    it "returns http success" do
-      get 'sign_out'
-      response.should be_success
+  context "POST 'login'" do
+    subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
+    it "Redirect to it role url" do
+      post :login, { 'user' => { :password => '123', :email => '11@1.1' } }
+      expect(response).to redirect_to('/publishers')
     end
   end
-=end
 end
