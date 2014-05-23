4c7e0874f5bc0017053abd8d86f90da54aa2dce7
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-22 23:40:20 -0300

    SPec test if the root redirect depending on the current user role

--- spec/controllers/redirect_controller_spec.rb
+++ spec/controllers/redirect_controller_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
+require 'factory_girl_rails'
 
 describe RedirectController do
+  describe "Root aplication should respond the role URL" do
+    context "#Publisher" do
+      subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
+      subject!(:publisher_profile_session) { session['user_id'] = publisher_profile.user.id }
+      it "should responde #/publishers INDEX" do
+        get :index
+        expect(response).to redirect_to('/publishers')
+      end
+    end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    context "#Advertiser" do
+      subject!(:advertiser_profile) { FactoryGirl.create(:advertiser_profile) }
+      subject!(:advertiser_profile_session) { session['user_id'] = advertiser_profile.user.id }
+      it "should responde #/advertisers INDEX" do
+        get :index
+        expect(response).to redirect_to('/advertisers')
+      end
     end
   end
-
 end
