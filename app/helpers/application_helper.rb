76d91494ad5f3de6e8d4abc684900d63fc8d57a6
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-27 00:45:36 -0300

    dynamic profile type name on the URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,7 +33,7 @@ module ApplicationHelper
 
     # Commum actions
     users_actions = [
-        { :name => t(:user_action)[:config_acc], :path => '/users/profiles/', :icon => 'fa fa-gears' },
+        { :name => t(:user_action)[:config_acc], :path => '/users/profiles/'+role_name, :icon => 'fa fa-gears' },
         { :name => t(:user_action)[:feedback], :path => '#feedback', :icon => 'fa fa-comments', :toggle => 'modal' }
     ]
 
