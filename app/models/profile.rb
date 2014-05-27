32f5749173fca84a869e260286018e42167f77f5
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-27 01:09:38 -0300

    Enums to constant roles id

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,9 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   belongs_to :role
 
+  # Constants
+  enum role_name: { publisher: 1, advertiser: 2, admin: 3 }
+
   def self.get_default_role profiles
     profiles.each do |profile|
       if profile.default_role then return profile.role.name end
