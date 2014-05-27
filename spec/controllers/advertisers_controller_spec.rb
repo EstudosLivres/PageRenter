ce41d825aade27fc441fe6a79c8496f1ec459552
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-27 14:39:43 -0300

    User session added (without the session the application redirect to the landing page)

--- spec/controllers/advertisers_controller_spec.rb
+++ spec/controllers/advertisers_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe AdvertisersController do
+  subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
+  subject!(:publisher_profile_session) { session['user_id'] = publisher_profile.user.id }
 
   describe "GET 'index'" do
     it "returns http success" do
