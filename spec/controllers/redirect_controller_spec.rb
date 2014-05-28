cc2dbad5640631f0f58059bee7052068e887e067
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-27 22:13:50 -0300

    symbol action updated (index to redirect_index)

--- spec/controllers/redirect_controller_spec.rb
+++ spec/controllers/redirect_controller_spec.rb
@@ -7,7 +7,7 @@ describe RedirectController do
       subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
       subject!(:publisher_profile_session) { session['user_id'] = publisher_profile.user.id }
       it "should responde #/publishers INDEX" do
-        get :index
+        get :redirect_index
         expect(response).to redirect_to('/publishers')
       end
     end
@@ -16,7 +16,7 @@ describe RedirectController do
       subject!(:advertiser_profile) { FactoryGirl.create(:advertiser_profile) }
       subject!(:advertiser_profile_session) { session['user_id'] = advertiser_profile.user.id }
       it "should responde #/advertisers INDEX" do
-        get :index
+        get :redirect_index
         expect(response).to redirect_to('/advertisers')
       end
     end
@@ -25,7 +25,7 @@ describe RedirectController do
       subject!(:publisher_profile) { FactoryGirl.create(:publisher_profile) }
       subject!(:publisher_profile_session) { session['user_id'] = publisher_profile.user.id }
       it "should responde #/publishers INDEX" do
-        get :index
+        get :redirect_index
         expect(response).to redirect_to('/publishers')
       end
     end
@@ -33,7 +33,7 @@ describe RedirectController do
     context "#InvalidRole" do
       subject!(:user) { FactoryGirl.create(:user) }
       it "should responde #/advertisers INDEX" do
-        get :index
+        get :redirect_index
         expect(response).to redirect_to(ApplicationController.land_url)
       end
     end
