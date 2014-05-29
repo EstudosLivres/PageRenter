afc938741d90daeffba16de2747fb2122f7300fa
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-29 12:46:06 -0300

    SignOut tested

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,10 +20,17 @@ describe UsersController do
 
   context "GET #SIGNOUT" do
     it "Should delete the session" do
+      session[:user_id] = 1
+      session[:user_idiom] = 'pt'
+      session[:urls_visited] = [{ date: '22/08/2013', advertise_id: 32 }]
+
       get :sign_out
 
       session[:user_id].should be_nil
       session[:user_idiom].should be_nil
+      session[:urls_visited].should_not be_nil
+      session[:urls_visited].should be_a(Array)
+      expect(response).to redirect_to(ApplicationController.land_url)
     end
   end
 end
