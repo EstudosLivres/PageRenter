f39a12e43d852488bcf407f91a45f2671ccc0eb7
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-01 17:16:27 -0300

    AdvertisingPiece (peça publicitária) adicionada ao Hash criado no form da campanha

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -71,6 +71,6 @@ class CampaignsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def campaign_params
-      params.require(:campaign).permit(:name, :redirect_link, :slogan, :description, :social_phrase)
+      params.require(:campaign).permit(:name, :redirect_link, :slogan, :description, :social_phrase, :advertising_piece)
     end
 end
