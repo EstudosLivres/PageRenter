1f4857defb1f22e0da01d46b1b2ef3168264b41a
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-04 08:58:37 -0300

    remove the validations for Slogan and social_phrase cause they are not required

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -6,9 +6,9 @@ class Campaign < ActiveRecord::Base
   # Rails validations
   validates :name, presence: true, length: { in: 3..50 }, on: [:create, :update]
   validates :redirect_link, presence: true, length: { in: 15..200 }, on: [:create, :update]
-  validates :slogan, presence: false, length: { in: 5..65 }
-  validates :description, presence: false, length: { in: 5..140 }
-  validates :social_phrase, presence: false, length: { in: 5..140 }
+  validates :slogan, presence: false # Slogan is not required
+  validates :description, presence: true, length: { in: 5..140 }
+  validates :social_phrase, presence: false # Social phrase is not required
   validates :user_id, presence: true
   validates_attachment_content_type :advertising_piece, :content_type => /\Aimage\/.*\Z/
 end
