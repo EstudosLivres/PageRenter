ba131c1899c1624927dfdc4ed999406b4e69626d
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-04 09:44:35 -0300

    Function to load the image when it is loaded (selected)

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -1,3 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+
+function show_campaign_image(input) {
+    if (input.files && input.files[0])
+    {
+        var reader = new FileReader();
+        reader.onload = function (e)
+        {
+            $('#new_campaign_image')
+                .attr('src',e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
