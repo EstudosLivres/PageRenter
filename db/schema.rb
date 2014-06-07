2dc443742bb89628504225bf6ba48d155f8067ad
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-07 10:23:57 -0300

    Added nick & email to be unique

--- db/schema.rb
+++ db/schema.rb
@@ -129,4 +129,7 @@ ActiveRecord::Schema.define(version: 20140605024546) do
     t.datetime "updated_at"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["nick"], name: "index_users_on_nick", unique: true, using: :btree
+
 end
