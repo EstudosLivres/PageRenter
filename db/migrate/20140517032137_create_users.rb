6f3e052ddfd7b8a704ea7963d64d6836639289ca
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-18 13:41:51 -0300

    user email unique

--- db/migrate/20140517032137_create_users.rb
+++ db/migrate/20140517032137_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name, limit: 55, null: false
       t.string :nick, limit: 30, null: false
-      t.string :email, limit: 55, null: false
+      t.string :email, limit: 55, unique: true, null: false
       t.column :locale, "char(5)", null: false
       t.column :pass_salt, "char(29)", null: true
       t.column :password, "char(60)", null: true
