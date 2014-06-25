c7b540b3d6407575b1a9d382b743f849c5958ee1
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-25 02:12:22 -0300

    renamed from transaction to bank_transaction

--- spec/factories/bank_transactions.rb
+++ spec/factories/bank_transactions.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :transaction do
+  factory :bank_transaction do
     value "9.99"
     currency "MyString"
     banking false
