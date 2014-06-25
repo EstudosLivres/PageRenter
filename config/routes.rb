2fadfed51b5087f28c5766c80b84c261b4d27d76
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-25 01:25:18 -0300

    TODO remember: campaigns index show the current user campaigns, only.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ PageRenter::Application.routes.draw do
   scope '/advertisers' do
     get '' => 'advertisers#index'
     get '/edit' => 'advertisers#edit'
-    resources :campaigns, except: :index
+    resources :campaigns # TODO remember: campaigns index show the current user campaigns, only.
 
     namespace :campaigns do
       resources :transactions
