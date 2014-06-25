3b81c8ba97024df8c728aa053e71eb5344ca3019
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-25 01:30:05 -0300

    actions hash converted into var

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,16 +30,29 @@ module ApplicationHelper
   end
 
   def role_actions
+    # Publisher menu_actions
+    new_social_session = { :name => t(:publisher_action)[:new_social_session], :path => '#new_social_session', :icon => 'fa fa-plus', :toggle => 'modal' }
+    publisher_invite = { :name => t(:publisher_action)[:invite], :path => '#invite', :icon => 'fa fa-paper-plane', :toggle => 'modal' }
+
+    # Advertiser menu_actions
+    new_campaign = { :name => t(:advertiser_action)[:new_campaign], :path => new_campaign_url, :icon => 'fa fa-plus', :toggle => 'modal' }
+    recommend = { :name => t(:advertiser_action)[:invite], :path => '#recommend', :icon => 'fa fa-paper-plane', :toggle => 'modal' }
+
+    # Universal User menu_actions
+    manage_acc = { :name => t(:user_action)[:manage_acc], :path => "/#{role_name.pluralize}/edit", :icon => 'fa fa-gears' }
+    api = { :name => 'API', :path => '/api/console/docs', :icon => 'fa fa-cubes' }
+    feedback = { :name => t(:user_action)[:feedback], :path => '#feedback', :icon => 'fa fa-comments', :toggle => 'modal' }
+
     case role_name
       when 'publisher'
         actions = [
-            { :name => t(:publisher_action)[:new_social_session], :path => '#new_social_session', :icon => 'fa fa-plus', :toggle => 'modal' },
-            { :name => t(:publisher_action)[:invite], :path => '#invite', :icon => 'fa fa-paper-plane', :toggle => 'modal' }
+            new_social_session,
+            publisher_invite
         ]
       when 'advertiser'
         actions = [
-            { :name => t(:advertiser_action)[:new_campaign], :path => new_campaign_url, :icon => 'fa fa-plus', :toggle => 'modal' },
-            { :name => t(:advertiser_action)[:invite], :path => '#recommend', :icon => 'fa fa-paper-plane', :toggle => 'modal' }
+            new_campaign,
+            recommend
         ]
     else
       actions = []
@@ -47,9 +60,9 @@ module ApplicationHelper
 
     # Commum actions
     users_actions = [
-        { :name => t(:user_action)[:manage_acc], :path => "/#{role_name.pluralize}/edit", :icon => 'fa fa-gears' },
-        { :name => 'API', :path => '/api/console/docs', :icon => 'fa fa-cubes' },
-        { :name => t(:user_action)[:feedback], :path => '#feedback', :icon => 'fa fa-comments', :toggle => 'modal' }
+        manage_acc,
+        api,
+        feedback
     ]
 
     unless actions.empty?
