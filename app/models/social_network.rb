3ba7cb3fa7b7c9b0f97ef24cec9ee7db844fd25d
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-28 01:05:54 -0300

    description length validation removed

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -5,7 +5,6 @@ class SocialNetwork < ActiveRecord::Base
   # Attrs validations
   validates :name, presence: true, length: { in: 3..50 }, on: [:create, :update]
   validates :acronym, length: { in: 3..10 }, on: [:create, :update]
-  validates :description, length: { in: 3..45 }, on: [:create, :update]
 
   # Validates Associations
 end
