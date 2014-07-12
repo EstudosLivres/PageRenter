dd7f860d853de31088ae4fd7da7439c0e0ab8436
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-11 23:43:27 -0300

    SetUp the SocialSession (create an instance based on the hash) & the add_it_to that add it social_session to especific user

--- app/models/social_session.rb
+++ app/models/social_session.rb
@@ -17,12 +17,41 @@ class SocialSession < ActiveRecord::Base
   validates :user_id, presence: true, on: [:create, :update]
   validates :social_network_id, presence: true, on: [:create, :update]
 
+  # Add the current SocialSession to the passed user
+  def add_it_to(user)
+    user.social_sessions << self
+  end
+
+  # Convert social_hash into social session instance
+  def self.setup(social_hash)
+    social_for_instantiation = social_hash['social_session']
+    login = social_for_instantiation['login']
+    pages = social_for_instantiation['pages']['data']
+
+    # Prepare social session hash
+    login['id_on_social'] = login['id']
+    login['social_network_id'] = login['network_id']
+    login.delete('network_id')
+    login.delete('id')
+    social_session = SocialSession.new(login.except('role'))
+
+    # Prepare page account hash
+    pages.each do |page|
+      # Change page attrs social to our DB
+      page['id_on_social'] = page['id']
+      page.delete('id')
+
+      social_session.page_accounts << PageAccount.new(page.except('perms'))
+    end
+
+    social_session
+  end
+
   # Convert social_hash into user_hash
   def self.to_user(social_hash)
     user_hash = social_hash['social_session']['login']
     user_hash['role'] = 'publisher'
-    user_hash['nick'] = user_hash['username']
-    user_hash.except('count_friends', 'id', 'network_id', 'username')
+    user_hash.except('count_friends', 'id', 'network_id')
   end
 
   # Authenticate the social session based on it specific rules
