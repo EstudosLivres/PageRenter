5446411c70185710cdbf74b55e9f38085cecc88e
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-12 23:06:01 -0300

    Return nil (do not try to iterate if the param is nil)

--- app/models/concerns/Util.rb
+++ app/models/concerns/Util.rb
@@ -4,6 +4,7 @@ module Concerns
 
     # Convert action controller into hash
     def self.action_controller_to_hash action_controller
+      return nil if action_controller.nil?
       return_hash = {}
 
       # Foreach the Actionc ontroller keys (converting to symbol & saving each value)
