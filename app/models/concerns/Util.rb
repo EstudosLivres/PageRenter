36d813bed4f278a912aa94c52e1b39bba3d60cdc
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-12 22:58:49 -0300

    concern Util with Util Static Methods that help on rails possible bugs (like ActionParams not instantiating like a Hash does, so it is converting ActionParams into Hash).

---
+++ app/models/concerns/Util.rb
@@ -0,0 +1,15 @@
+module Concerns
+  class Util
+    extend ActiveSupport::Concern
+
+    # Convert action controller into hash
+    def self.action_controller_to_hash action_controller
+      return_hash = {}
+
+      # Foreach the Actionc ontroller keys (converting to symbol & saving each value)
+      action_controller.each {|key, value| return_hash[key.to_sym] = value}
+
+      return_hash
+    end
+  end
+end
