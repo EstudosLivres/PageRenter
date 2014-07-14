31dbbf05ea91b8ef8b0edbea8ef9eb81940a1892
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-14 20:37:44 -0300

    Base EDIT/UPDATE profile added.

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,30 @@
+class ProfileController < ApplicationController
+  # GET /profiles/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /profiles/1
+  # PATCH/PUT /profiles/1.json
+  def update
+    respond_to do |format|
+      if @profile.update(profile_params)
+        format.html { redirect_to @profile, notice: 'profile was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def profile_params
+    params.require(:profile).permit(:name, :redirect_link, :slogan, :description, :social_phrase, :advertising_piece)
+  end
+end
