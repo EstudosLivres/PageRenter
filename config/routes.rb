c2a430d1debe92577648388cea9d6ccaca086d12
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-14 21:59:01 -0300

    profile controller removed

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ PageRenter::Application.routes.draw do
   scope '/publishers' do
     get '' => 'publishers#index'
     get '/edit' => 'publishers#edit'
-    resource :profile, only: [:edit, :update]
   end
 
   # Actions Under/For Advertiser
@@ -23,7 +22,6 @@ PageRenter::Application.routes.draw do
     get '' => 'advertisers#index'
     get '/edit' => 'advertisers#edit'
     resources :campaigns # TODO remember: campaigns index show the current user campaigns, only.
-    resource :profile, only: [:edit, :update]
 
     namespace :campaigns do
       # TODO resources :financial_transaction
