82698f03a680421865b69afff00ccbbca5660a40
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-14 23:24:10 -0300

    PUT & EDIT action for the Controllers + callbacks (private)

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -1,10 +1,38 @@
 class AdvertisersController < ApplicationController
   # Prevent spam USER.FIND on DB
   before_action :setup_user
+  before_action :set_advertiser
 
+  # Root for Advertiser user
   def index
   end
 
+  # Configuration for Advertiser user
   def edit
+  end
+
+  # PATCH/PUT /advertiser/1
+  # PATCH/PUT /advertiser/1.json
+  def update
+    respond_to do |format|
+      if @advertiser.update(advertiser_params)
+        format.html { redirect_to @advertiser, notice: 'Advertiser was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @advertiser.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_advertiser
+    @advertiser = @current_user.advertiser
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def advertiser_params
+    params.require(:profile).permit(:name,:avatar)
   end
 end
--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,11 +1,39 @@
 class PublishersController < ApplicationController
   # Prevent spam USER.FIND on DB
   before_action :setup_user
+  before_action :set_publisher
 
+  # Root for Publisher user
   def index
     @campaigns = Campaign.all
   end
 
+  # Configuration for Publisher user
   def edit
+  end
+
+  # PATCH/PUT /publisher/1
+  # PATCH/PUT /publisher/1.json
+  def update
+    respond_to do |format|
+      if @publisher.update(publisher_params)
+        format.html { redirect_to @publisher, notice: 'Publisher was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @publisher.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_publisher
+    @publisher = @current_user.publisher
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def publisher_params
+    params.require(:publisher).permit(:name)
   end
 end
