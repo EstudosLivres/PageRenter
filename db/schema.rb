1b9ba87cf2b12ea8e174f4d1599ba6400f9a2318
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-14 21:58:33 -0300

    avatar added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605024546) do
+ActiveRecord::Schema.define(version: 20140714221751) do
 
   create_table "campaigns", force: true do |t|
     t.string   "name",                           limit: 50,  null: false
@@ -69,10 +69,14 @@ ActiveRecord::Schema.define(version: 20140605024546) do
   end
 
   create_table "profiles", force: true do |t|
-    t.string   "name",         limit: 55
-    t.boolean  "default_role",            null: false
-    t.integer  "role_id",                 null: false
+    t.string   "name",                limit: 55
+    t.boolean  "default_role",                   null: false
+    t.integer  "role_id",                        null: false
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
