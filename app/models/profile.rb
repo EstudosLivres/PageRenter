e974248e4eacd918d12171b2f2ef005cd61ac896
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-15 14:50:36 -0300

    Size of UPLoad Logo REFACTORED!

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
   # Relations
   belongs_to :user
   belongs_to :role
-  has_attached_file :avatar, :styles => { :medium => '470x300>', :thumb => '117x75>' }, :default_url => '/images/:style/missing.png'
+  has_attached_file :avatar, :styles => { :medium => '300x75>', :thumb => '100x15>' }, :default_url => '/images/:style/missing.png'
 
   # Validates Associations
   validates :user_id, presence: true, on: :update
