c3694dc3ff06e9796706328b941f0ecac0b10d8b
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-18 20:55:47 -0300

    - slogan removed - title added (means which appears to call attention from the publisher) - description can't be null

--- db/migrate/20140529173154_create_campaigns.rb
+++ db/migrate/20140529173154_create_campaigns.rb
@@ -2,10 +2,10 @@ class CreateCampaigns < ActiveRecord::Migration
   def change
     create_table :campaigns do |t|
       t.string :name, limit: 50, null: false
-      t.string :redirect_link, limit: 200, null: false
-      t.string :slogan, limit: 65, null: true
-      t.string :description, limit: 140, null: true
+      t.string :title, limit: 25, null: false
+      t.string :description, limit: 140, null: false
       t.string :social_phrase, limit: 140, null: true
+      t.string :redirect_link, limit: 200, null: false
       t.references :advertiser, index: true
 
       t.timestamps
