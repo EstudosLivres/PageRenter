bb481ddbd85d64cc135f3bef83352f204fe22070
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-19 00:23:40 -0300

    validations added

--- app/models/campaign_history_state.rb
+++ app/models/campaign_history_state.rb
@@ -1,4 +1,9 @@
 class CampaignHistoryState < ActiveRecord::Base
+  # Relations
   belongs_to :campaign
   belongs_to :campaign_state
+
+  # Validates Associations
+  validates :campaign_id, presence: true, on: [:create, :update]
+  validates :campaign_state_id, presence: true, on: [:create, :update]
 end
--- app/models/campaign_state.rb
+++ app/models/campaign_state.rb
@@ -1,4 +1,8 @@
 class CampaignState < ActiveRecord::Base
+  # Relations
   has_many :campaign_history_states
   has_many :campaigns, through: :campaign_history_states
+
+  # Rails validations
+  validates :name, presence: true, length: { in: 3..40 }, on: [:create, :update]
 end
