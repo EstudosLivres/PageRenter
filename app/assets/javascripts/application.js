665264ee188fd1d4ac13a64a1fa6dfc5b94d4442
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-23 23:31:21 -0300

    turbolink removed, scaffold sas removed & campaign form calendar example working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require turbolinks
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-datepicker/core
