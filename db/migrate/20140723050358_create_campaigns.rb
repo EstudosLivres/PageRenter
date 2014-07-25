1efa5e2a8cbd4b0efc850161eca5ad8ae41c92e0
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-25 16:09:03 -0300

    campaign attrs UPDATED

--- db/migrate/20140723050358_create_campaigns.rb
+++ db/migrate/20140723050358_create_campaigns.rb
@@ -1,7 +1,10 @@
 class CreateCampaigns < ActiveRecord::Migration
   def change
     create_table :campaigns do |t|
-      t.string :name
+      t.string :name, limit: 75, null: false
+      t.string :launch_date, limit: 10, null: false
+      t.string :end_date, limit: 10, null: false
+      t.belongs_to :user
 
       t.timestamps
     end
