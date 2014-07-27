f5a6db749d6b79fb2d76cf702380e223372769b3
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-27 02:04:45 -0300

    Validating that the EndDate must be greater than LauncheDate

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -6,7 +6,18 @@ class Campaign < ActiveRecord::Base
   belongs_to :advertiser, class_name: 'Profile', foreign_key: :advertiser_id
 
   # Rails validations
+  validate :validate_end_date_before_launch_date, on: [:create, :update]
   validates :name, presence: true, length: { in: 5..75 }, on: [:create, :update]
   validates :launch_date, presence: true, on: [:create, :update]
   validates :end_date, presence: true, on: [:create, :update]
+
+  # Validate the End be greater than Launch
+  def validate_end_date_before_launch_date
+    if launch_date && end_date
+      if launch_date >= end_date
+        self.errors.add(:end_date, 'End date should be greater than launch date')
+        raise ActiveRecord::Rollback
+      end
+    end
+  end
 end
