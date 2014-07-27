94b83fa04ed1d01de33fda3c00fce81a7d5e37c4
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-27 00:14:28 -0300

    validations & relations added

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,2 +1,12 @@
 class Campaign < ActiveRecord::Base
+  # Relations
+  has_many :ads
+  has_many :budgets
+  belongs_to :user
+  belongs_to :advertiser, class_name: 'User', foreign_key: :advertiser_id
+
+  # Rails validations
+  validates :name, presence: true, length: { in: 5..75 }, on: [:create, :update]
+  validates :launch_date, presence: true, on: [:create, :update]
+  validates :end_date, presence: true, on: [:create, :update]
 end
