e96b59902cf39eb81c8f9d8bb4d28307dcaf407a
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-27 00:48:30 -0300

    campaign referece the profile, not the user

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,7 +3,7 @@ class Campaign < ActiveRecord::Base
   has_many :ads
   has_many :budgets
   belongs_to :user
-  belongs_to :advertiser, class_name: 'User', foreign_key: :advertiser_id
+  belongs_to :advertiser, class_name: 'Profile', foreign_key: :advertiser_id
 
   # Rails validations
   validates :name, presence: true, length: { in: 5..75 }, on: [:create, :update]
