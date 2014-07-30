dd056331d6f966a59583a9c6337f85521aeda2d2
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-29 23:19:38 -0300

    better understandable the currency & those acronym

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,10 @@
 class Currency < ActiveRecord::Base
   has_many :ad_pricings
   has_many :financial_transactions
+
+  # Aux methods to return better understandable
+  def acronym_with_name
+    name = I18n.t("db_enum.currencies.#{self.name}")
+    "#{self.acronym} - #{name}"
+  end
 end
