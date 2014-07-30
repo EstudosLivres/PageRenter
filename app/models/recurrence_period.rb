546dd7b869099a9b0b95e862c2206d08baa17ed2
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-29 23:58:13 -0300

    method to better print on the view (readable) the hardcoded elements on the Database

--- app/models/recurrence_period.rb
+++ app/models/recurrence_period.rb
@@ -1,2 +1,11 @@
 class RecurrencePeriod < ActiveRecord::Base
+  has_many :budgets
+
+  # Rails validations
+  validates :name, presence: true, length: { in: 5..15 }, on: [:create, :update]
+
+  # Aux methods to return better understandable
+  def translated_recurrence_name
+    I18n.t("db_enum.recurrence_periods.#{self.name}")
+  end
 end
