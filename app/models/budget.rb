4e6f2dc646e7b0418f8d6a769b2260b237e4a9be
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-30 13:00:04 -0300

    method to disable the other budgets on the history

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,18 +2,28 @@ class Budget < ActiveRecord::Base
   belongs_to :currency
   belongs_to :campaign
   belongs_to :recurrence_period
+  # TODO has_many financial_transaction trough BudgetLaunches
 
   # Custom validations
+  validate :disable_last_budget, on: [:create] # TODO verify how does it work
 
   # Rails validations
-  validates :activated, presence: true, on: [:create, :update]
   validates :value, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 1, :less_than => 100000000}, on: [:create, :update]
   validates :closed_date, presence: true, on: :update
 
   # Validates Associations
-  validates :currency_id, presence: true, on: [:create, :update]
-  validates :campaign_id, presence: true, on: [:create, :update]
-  validates :recurrence_period_id, presence: true, on: [:create, :update]
+  validates :currency_id, presence: true, on: [:create]
+  validates :campaign_id, presence: true, on: [:create]
+  validates :recurrence_period_id, presence: true, on: [:create]
+
+  # The budget have a history, so can't be UPDATED neither be DESTROYED
+  def disable_last_budget
+    last_budget = self.campaign.budgets.last
+    unless last_budget.nil?
+      last_budget.update(activated:false, closed_date:Time.now)
+      # TODO prevent to persist if the last budget couldn't be UPDATED
+    end
+  end
 
   # TODO activated significa que é o orçamento corrente este que o usuário está usando
   # TODO lembrar que os orçamentos nunca são editados, sempre são criados novos
