7e64e69adfd52d0fe62ef3cc22eae96a3dc40713
Ilton Garcia <ilton_unb@hotmail.com>
2014-07-31 15:12:45 -0300

    timezone note

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module PageRenter
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Brasilia'
+    # See PERSONAL NOTE
+    config.time_zone = 'Brasilia' # Rails still saving for +000 zone, but when asked for the the attr it is converted
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
