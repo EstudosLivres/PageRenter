f8469a920186d1b63ecd3d67a5169346832729f4
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-07 23:26:47 -0300

    validate username attr

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -5,6 +5,7 @@ class SocialNetwork < ActiveRecord::Base
   # Attrs validations
   validates :name, presence: true, length: { in: 3..50 }, on: [:create, :update]
   validates :acronym, length: { in: 2..10 }, on: [:create, :update]
+  validates :username, length: { in: 2..25 }, on: [:create, :update]
 
   # Validates Associations
 end
