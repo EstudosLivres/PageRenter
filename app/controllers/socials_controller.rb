9bc409303d5df236cd5a4ddbc373aea4ce8dda0d
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-12 14:04:26 -0300

    Redirect to facebook signup if there is no code sent

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -1,5 +1,14 @@
 class SocialsController < ApplicationController
   def auth
+    # Initializing the env
+    network_class = params[:social_network_name].humanize
+    network_obj = "Socials::#{network_class}".constantize.new
+    return redirect_to network_obj.sign_up if params[:code].nil?
+    session[:fb_access_token] = network_obj.set_up_graph(params[:code])
+
+    # Setting the vars to be used on the View
+    @network_login_url = network_obj.sign_up
+    @network_login_url
     render :layout => 'blank'
   end
 end
