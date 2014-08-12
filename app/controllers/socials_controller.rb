aaf49b340e91eeeb66298aa665fbee81f6b20b10
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-12 14:09:50 -0300

    commented if case & TODO added

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -3,12 +3,12 @@ class SocialsController < ApplicationController
     # Initializing the env
     network_class = params[:social_network_name].humanize
     network_obj = "Socials::#{network_class}".constantize.new
+
+    # return to the sign_up page if there is no OAuth code
     return redirect_to network_obj.sign_up if params[:code].nil?
     session[:fb_access_token] = network_obj.set_up_graph(params[:code])
 
-    # Setting the vars to be used on the View
-    @network_login_url = network_obj.sign_up
-    @network_login_url
+    # TODO just call the user persist based on social_hash
     render :layout => 'blank'
   end
 end
