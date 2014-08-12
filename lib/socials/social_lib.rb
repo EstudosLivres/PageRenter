00f8acb0ca526f81d74e25df7076a2b37edaf9bd
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-11 23:17:31 -0300

    base methods to be implemented added

---
+++ lib/socials/social_lib.rb
@@ -0,0 +1,16 @@
+module Socials
+  class SocialLib
+    # Config the app for the connection
+    def setup(setup_hash) end
+    # Redirect the user to the SocialNetwork SignUp page
+    def sign_up(sign_up_hash) end
+    # Get the user Logged hash & accesses (OAuth)
+    def get_current_user() end
+    # Get the Multi logins from the user (Pages, in Facebook case)
+    def get_user_admin_logins() end
+    # Retrieve the user accepted permissions on the SocialNetwork
+    def get_active_permission() end
+    # Share in Facebook OR tweet on Twitter
+    def send_msg(msg, link) end
+  end
+end
