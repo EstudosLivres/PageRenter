0202b5ec28643fc705018abf68ce82c26b5c007f
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-12 21:05:11 -0300

    call the persistence & return the erro (MUST BE REFACTORED yet)

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -6,9 +6,15 @@ class SocialsController < ApplicationController
 
     # return to the sign_up page if there is no OAuth code
     return redirect_to network_obj.sign_up if params[:code].nil?
-    session[:fb_access_token] = network_obj.set_up_graph(params[:code])
+    fb_access_token = network_obj.set_up_graph(params[:code])
+    session[:fb_access_token] = fb_access_token if session[:fb_access_token].nil?
+    user = SocialNetwork.persist_social_user(network_obj)
 
-    # TODO just call the user persist based on social_hash
+    # Verify if there is any error
+    unless user.errors.empty?
+      return render html: {notice: user.errors} if user.errors.messages.is_a?(Hash)
+    end
+
     render :layout => 'blank'
   end
 end
