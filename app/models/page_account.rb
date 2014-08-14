c365a2047f171934bd94418d07a06fb744f61157
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-13 23:15:34 -0300

    validate those attrs

--- app/models/page_account.rb
+++ app/models/page_account.rb
@@ -6,6 +6,9 @@ class PageAccount < ActiveRecord::Base
   validates :id_on_social, presence: true, length: { in: 3..45 }, on: [:create, :update]
   validates :name, presence: true, length: { in: 3..75 }, on: [:create, :update]
   validates :category, presence: true, length: { in: 3..25 }, on: [:create, :update]
+  validates :followers, presence: true, on: [:create, :update]
+  validates :local_interaction, presence: true, on: [:create, :update]
+  validates :foreign_interaction, presence: true, on: [:create, :update]
 
   # Validates Associations
 end
