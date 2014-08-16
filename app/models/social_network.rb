bbcc4b7be2cf73ba7610bcf738bd88a8f65ecbd2
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-16 03:17:54 -0300

    authenticating user by a simple join

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -15,6 +15,14 @@ class SocialNetwork < ActiveRecord::Base
     # Get the class (name after the module => 'module::class')
     obj_instance = /[^::]*$/.match("#{network_obj.class}")
     obj_instance_str = "#{obj_instance}".downcase
+
+    # About the authentication (to prevent to retrieve all data from the user again)
+    base_user_hash = network_obj.get_base_user
+    # Authenticate the user TODO: is it wrong? if if using OAuth should I remove the validation on SocialSession which was for JS or wait need it on JS?
+    user = User.joins(:social_sessions).where('users.email'=>base_user_hash[:email],'social_sessions.email'=>base_user_hash[:email]).take
+    return user unless user.nil?
+
+    # Retrieving the user data
     social_obj = network_obj.get_current_user
     social_obj[:social_network] = obj_instance_str
 
