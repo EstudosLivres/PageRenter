21eef3f497c3b4588ba181c49a49281664b9f1b9
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-19 11:54:47 -0300

    ignore aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config({
-  :access_key_id => 'REPLACE_WITH_ACCESS_KEY_ID',
-  :secret_access_key => 'REPLACE_WITH_SECRET_ACCESS_KEY',
+  :access_key_id => 'xpto',
+  :secret_access_key => 'xpto_secreto',
 })
