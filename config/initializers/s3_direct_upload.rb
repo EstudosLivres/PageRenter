d6120b11caeabaf73a87852438a164de1755d69e
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-19 13:49:14 -0300

    config S3 Direct using the data from aws.yml

---
+++ config/initializers/s3_direct_upload.rb
@@ -0,0 +1,14 @@
+S3DirectUpload.config do |c|
+  aws_config = YAML.load_file("#{Rails.root.to_s}/config/aws.yml")
+  aws_dev = aws_config['development']
+  aws_prod = aws_config['production']
+
+  # TODO substituir quando for pra produção
+  aws = aws_dev
+
+  c.access_key_id     = aws['access_key_id']
+  c.secret_access_key = aws['secret_access_key']
+  c.bucket            = aws['bucket']
+  c.region            = aws['region']
+  c.url = "https://#{c.bucket}.s3.amazonaws.com/"
+end
