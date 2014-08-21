3190ea7a20ac333e91d7d5532f85e6fcaedb5b02
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-21 10:42:46 -0300

    Create the user on a different Thread to do not lock the server & social_persisting is a layout to be like CVC screen

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -8,14 +8,18 @@ class SocialsController < ApplicationController
     return redirect_to network_obj.sign_up if params[:code].nil?
     fb_access_token = network_obj.set_up_graph(params[:code])
     session[:fb_access_token] = fb_access_token if session[:fb_access_token].nil?
-    user = SocialNetwork.persist_social_user(network_obj)
 
-    # Verify if there is any error
-    return render html: {notice: user.errors} if user.errors.messages.is_a?(Hash) if !user.errors.empty?
+    # Create the user on a different Thread to do not lock the server
+    Thread.new do
+      user = SocialNetwork.persist_social_user(network_obj)
 
-    session[:user_id] = user.id
-    return redirect_to root_url
+      # Verify if there is any error
+      return render html: {notice: user.errors} if user.errors.messages.is_a?(Hash) if !user.errors.empty?
 
-    render :layout => 'blank'
+      session[:user_id] = user.id
+      return redirect_to root_url
+    end
+
+    render :layout => 'social_persisting'
   end
 end
