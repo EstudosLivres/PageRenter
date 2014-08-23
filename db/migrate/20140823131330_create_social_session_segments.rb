c8c85686beefd7c8266f33361f839b0fb8782b69
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-23 10:20:06 -0300

    social_session association with segments just created

--- db/migrate/20140823131330_create_social_session_segments.rb
+++ db/migrate/20140823131330_create_social_session_segments.rb
@@ -1,9 +1,9 @@
 class CreateSocialSessionSegments < ActiveRecord::Migration
   def change
     create_table :social_session_segments do |t|
-      t.belongs_to :segment, index:true, null:false
-      t.belongs_to :social_session, index:true, null:true
-      t.string :id_on_social,
+      t.belongs_to :segment, index: true, null: false
+      t.belongs_to :social_session, index: true, null: true
+      t.string :id_on_social, limit: 45, null: false
 
       t.timestamps
     end
