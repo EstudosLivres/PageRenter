3ac1481333613b34dbf2aaeadc3862a7c85c2c47
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-23 02:00:18 -0300

    hash attrs updated from 1.8.3 declaration to 1.9.1 (:name => to name:) & base admin actions added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,19 +29,27 @@ module ApplicationHelper
     if role_name == 'publisher' then return 'btn-success' else return 'btn-primary' end
   end
 
+  # Build the arrays of actions for the user menu
   def role_actions
     # Publisher action_menu
-    new_social_session = { :name => t(:publisher_action)[:new_social_session], :path => add_social_login_path, :icon => 'fa fa-plus', :toggle => '' }
-    publisher_invite = { :name => t(:publisher_action)[:invite], :path => '#invite', :icon => 'fa fa-paper-plane', :toggle => 'modal' }
+    new_social_session = { name: t(:publisher_action)[:new_social_session], path:add_social_login_path, icon:'fa fa-plus', toggle:'' }
+    publisher_invite = { name:t(:publisher_action)[:invite], path:'#invite', icon:'fa fa-paper-plane', toggle:'modal' }
 
     # Advertiser action_menu
-    new_campaign = { :name => t(:advertiser_action)[:new_campaign], :path => new_campaign_url, :icon => 'fa fa-plus', :toggle => 'modal' }
-    recommend = { :name => t(:advertiser_action)[:invite], :path => '#recommend', :icon => 'fa fa-paper-plane', :toggle => 'modal' }
+    new_campaign = { name:t(:advertiser_action)[:new_campaign], path:new_campaign_url, icon:'fa fa-plus', toggle:'modal' }
+    recommend = { name:t(:advertiser_action)[:invite], path:'#recommend', icon:'fa fa-paper-plane', toggle:'modal' }
+
+    # Admin action_menu
+    new_segmentation = {}
+    edit_segmentation = {}
+    base_segmentation = {name:'GerenciarSegmentos', path:admin_segmentation_path, icon:'fa pie_chart', toggle:''}
+    segmentation = [base_segmentation, new_segmentation, edit_segmentation]
+    analyse_ads = {name:'AnalisarAnúncios', path:admin_ad_analyse_path, icon:'fa fa-check-square-o', toggle:''}
 
     # Universal User action_menu
-    manage_acc = { :name => t(:user_action)[:manage_acc], :path => "/#{role_name.pluralize}/edit", :icon => 'fa fa-gears' }
-    api = { :name => 'API', :path => '/api/console/docs', :icon => 'fa fa-cubes' }
-    feedback = { :name => t(:user_action)[:feedback], :path => '#feedback', :icon => 'fa fa-comments', :toggle => 'modal' }
+    manage_acc = { name:t(:user_action)[:manage_acc], path:"/#{role_name.pluralize}/edit", icon:'fa fa-gears' }
+    api = { name:'API', path:'/api/console/docs', icon:'fa fa-cubes' }
+    feedback = { name:t(:user_action)[:feedback], path:'#feedback', icon:'fa fa-comments', toggle:'modal' }
 
     case role_name
       when 'publisher'
@@ -53,6 +61,11 @@ module ApplicationHelper
         actions = [
             new_campaign,
             recommend
+        ]
+      when 'admin'
+        actions = [
+          segmentation,
+          analyse_ads
         ]
     else
       actions = []
