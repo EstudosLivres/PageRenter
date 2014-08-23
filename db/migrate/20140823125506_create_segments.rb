d202594bdb9dc4886a9559791c7bd14879b7fdfa
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-23 10:14:55 -0300

    table attrs specified

--- db/migrate/20140823125506_create_segments.rb
+++ db/migrate/20140823125506_create_segments.rb
@@ -1,8 +1,8 @@
 class CreateSegments < ActiveRecord::Migration
   def change
     create_table :segments do |t|
-      t.string :name
-      t.string :description
+      t.string :name, limit: 75, null: false
+      t.string :description, limit: 140, null: true
 
       t.timestamps
     end
