f06795eb476d7a26ea5fb6b2d941913eb8d0da2a
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-25 16:08:02 -0300

    validations added (attrs)

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -4,5 +4,6 @@ class Segment < ActiveRecord::Base
   has_many :social_sessions, through: :social_session_segments
 
   # Rails validations
-
+  validates :name, presence: true, length: { in: 10..75 }, on: [:create, :update]
+  validates :description, presence: false, on: [:create, :update]
 end
