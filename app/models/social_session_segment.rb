7d49e8aeceecb2ac1dac412c9b8ee20d4a19a060
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-26 02:25:18 -0300

    association was redundant & the validation is now working (unique id_on_social)

--- app/models/social_session_segment.rb
+++ app/models/social_session_segment.rb
@@ -1,11 +1,10 @@
 class SocialSessionSegment < ActiveRecord::Base
   # Relations
   belongs_to :social_session
-  has_and_belongs_to_many :social_session_segments
+  has_and_belongs_to_many :segments
 
   # Rails validations
   validates :social_session_id, presence: false, on:[:create, :update]
-  validates :id_on_social, presence: true, length: { in: 10..45 }, on:[:create, :update]
-  validates_uniqueness_of :id_on_social, on:[:create, :update]
-  validates_uniqueness_of :social_session_id, on:[:create, :update]
+  validates :id_on_social, presence: true, length: { in: 1..45 }, on:[:create, :update]
+  validates_uniqueness_of :id_on_social, on: [:create, :update]
 end
