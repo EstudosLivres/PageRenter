349f68915b2a5584e57258e943387b057845b20c
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-26 01:10:47 -0300

    unique attrs added

--- db/migrate/20140823131330_create_social_session_segments.rb
+++ db/migrate/20140823131330_create_social_session_segments.rb
@@ -1,8 +1,8 @@
 class CreateSocialSessionSegments < ActiveRecord::Migration
   def change
     create_table :social_session_segments do |t|
-      t.belongs_to :social_session, index: true, null: true
-      t.string :id_on_social, limit: 45, null: false
+      t.belongs_to :social_session, index: true, unique:true, null: true
+      t.string :id_on_social, limit: 45, unique:true, null: false
 
       t.timestamps
     end
