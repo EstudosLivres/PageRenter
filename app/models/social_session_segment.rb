b18242351c4bb8e0094d1870d23d72d98b961e11
Ilton Garcia <ilton_unb@hotmail.com>
2014-08-26 01:10:17 -0300

    attrs rails validations

--- app/models/social_session_segment.rb
+++ app/models/social_session_segment.rb
@@ -2,4 +2,10 @@ class SocialSessionSegment < ActiveRecord::Base
   # Relations
   belongs_to :social_session
   has_and_belongs_to_many :social_session_segments
+
+  # Rails validations
+  validates :social_session_id, presence: false, on:[:create, :update]
+  validates :id_on_social, presence: true, length: { in: 10..45 }, on:[:create, :update]
+  validates_uniqueness_of :id_on_social, on:[:create, :update]
+  validates_uniqueness_of :social_session_id, on:[:create, :update]
 end
