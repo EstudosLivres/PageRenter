78dd36fa1dbd7312ca9411e5bdf5489022d10e61
Ilton Garcia <ilton_unb@hotmail.com>
2014-09-01 22:25:09 -0300

    persist user to prevent start thread and dont back to setup sessions

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -9,6 +9,20 @@ class SocialsController < ApplicationController
     fb_access_token = network_obj.set_up_graph(params[:code])
     session[:fb_access_token] = fb_access_token if session[:fb_access_token].nil?
 
+    # About the authentication (to prevent to retrieve all data from the user again)
+    base_user_hash = network_obj.get_base_user
+    # Authenticate the user
+    user = User.joins(:social_sessions).where('social_sessions.email'=>base_user_hash[:email]).take
+
+    # save on session if use retrieved
+    if !user.nil?
+      session[:user_id] = user.id
+      return redirect_to root_url
+    else
+      u = User.new(base_user_hash)
+      session[:user_id] = u.id if u.save
+    end
+
     # Create the user on a different Thread to do not lock the server
     Thread.new do
       user = SocialNetwork.persist_social_user(network_obj)
@@ -20,6 +34,6 @@ class SocialsController < ApplicationController
       return redirect_to root_url
     end
 
-    render :layout => 'social_persisting'
+    return redirect_to root_url unless session[:user_id].nil?
   end
 end
