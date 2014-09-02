b7a92caba31695feb0878b0c07374c2d9be1a5c5
Ilton Garcia <ilton_unb@hotmail.com>
2014-09-01 22:01:49 -0300

    RentS part removed

--- spec/factories/payment_methods.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :payment_method, :class => 'PaymentMethods' do
-    name "MyString"
-    method_type "MyString"
-    description "MyString"
-  end
-end
