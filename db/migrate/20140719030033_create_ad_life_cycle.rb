e242f01114acc96868cae1ea2d3b8488b2548a39
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-03 14:25:41 -0200

    null as false & attr removed

--- db/migrate/20140719030033_create_ad_life_cycle.rb
+++ db/migrate/20140719030033_create_ad_life_cycle.rb
@@ -3,7 +3,6 @@ class CreateAdLifeCycle < ActiveRecord::Migration
     # The states of the campaign life cycle
     create_table :ad_states do |t|
       t.string :name, limit: 40, null: false
-      t.string :msg, limit: 15, null: false
       t.string :description, limit: 140, null: true
 
       t.timestamps
@@ -11,9 +10,8 @@ class CreateAdLifeCycle < ActiveRecord::Migration
 
     # The history of when the campaign got the states
     create_table :ad_history_states do |t|
-      t.belongs_to :ad
-      t.belongs_to :ad_state
-      t.string :reason, limit: 140, null: true
+      t.belongs_to :ad, null: false
+      t.belongs_to :ad_state, null: false
 
       t.timestamps
     end
