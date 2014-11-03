11b8a64a54b7894df0f9bf73578900d0cddf7c33
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-03 14:49:07 -0200

    Attrs REFACTORED

--- db/migrate/20140605024545_create_financial_transactions.rb
+++ db/migrate/20140605024545_create_financial_transactions.rb
@@ -1,12 +1,16 @@
 class CreateFinancialTransactions < ActiveRecord::Migration
   def change
     create_table :financial_transactions do |t|
-      t.decimal :value, :decimal, precision: 9, scale: 2, null: false
-      t.string :currency, limit: 30, null: false
-      t.boolean :banking, null: false
-      t.belongs_to :payment_method, index: true
-      t.references :payer, index: true
-      t.references :receiver, index: true
+      # It value must be in integer syntax, like the operator requires (int and last 2 characters are the cents)
+      t.string :value, limit: 10, null: false
+      # Default is false = user_payment, true = user_receipt
+      t.boolean :withdraw, null: false
+      # Sent to the Gateway, but is saved just it string name
+      t.string :payment_method, limit: 50, index: true, null: false
+
+      # Associations
+      t.belongs_to :user, index: true, null: false
+      t.belongs_to :currency, index: true, null: false
 
       t.timestamps
     end
