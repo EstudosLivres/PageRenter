b11f48106f91b08c434ea9c97910f9154bc7c541
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-05 18:27:24 -0200

    admin login must be in custom layout for sign

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,6 +5,8 @@ class AdminsController < ApplicationController
 
   # GET /admins/login
   def login
+    # Custom layout, without navbar and anything else which need user logged
+    layout 'sign'
     redirect_to admin_root_url unless @current_user.nil?
   end
 end
