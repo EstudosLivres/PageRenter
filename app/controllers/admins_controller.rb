5500d3bced78ce3286119bddc18aaea58d447c9b
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-06 14:09:23 -0200

    sign layout only for it login action

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,12 +1,13 @@
 class AdminsController < ApplicationController
+  # Custom layout, without navbar and anything else which need user logged
+  layout 'sign', only: [:login]
+
   # GET /admins
   def index
   end
 
   # GET /admins/login
   def login
-    # Custom layout, without navbar and anything else which need user logged
-    layout 'sign'
     redirect_to admin_root_url unless @current_user.nil?
   end
 end
