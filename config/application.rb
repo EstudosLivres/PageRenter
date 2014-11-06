3e3800102758e15fcd3d80c2f64d294bc4a37afe
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-06 07:58:04 -0200

    Bundler.require REFACTORED, because I grouped all the gems in specific groups

--- config/application.rb
+++ config/application.rb
@@ -1,24 +1,35 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# ALL THOSE REQUIRES ARE NEEDED BECAUSE I'VE GROUPED EVERY GEM
+Bundler.require(*Rails.groups, :useful, :payment, :rails, :user, :services, :build_front_end, :style_and_interactions, :doc)
 
 module PageRenter
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Brasilia' # Rails still saving for +000 zone, but when asked for the the attr it is converted
+    # config.time_zone = 'Central Time (US & Canada)'
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.to_prepare do
+      Devise::SessionsController.layout proc{ |controller| action_name == 'new' ? "sign"   : "application" }
+    end
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :pt_BR
   end
 end
