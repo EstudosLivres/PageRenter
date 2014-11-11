4eccda032ab4ec3c4feda766d1243603c1889e8c
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-11 15:26:50 -0200

    check_profile encapsulated & trigger keyup to setup it avatar if it is using cookie/cache

--- app/assets/javascripts/sign.js
+++ app/assets/javascripts/sign.js
@@ -7,17 +7,25 @@ $(function(){
     avatar = $('#avatar');
     var login_input = $('.username_avatar');
     login_input.keyup(function(){
-        // Vars
-        login_value = this.value;
-        check_user_url = 'https://graph.facebook.com/'+login_value;
-        picture_url = 'https://graph.facebook.com/'+login_value+'/picture?type=large';
-
-        // Ajax
-        $.get(check_user_url)
-            .success(function(resp){profile_success(resp);})
-            .error(function(resp){profile_error(resp);});
+        check_profile_img(this);
     });
+
+    setTimeout(function(){
+        login_input.trigger('keyup');
+    }, 500);
 });
+
+function check_profile_img(input){
+    // Vars
+    login_value = input.value;
+    check_user_url = 'https://graph.facebook.com/'+login_value;
+    picture_url = 'https://graph.facebook.com/'+login_value+'/picture?type=large';
+
+    // Ajax
+    $.get(check_user_url)
+        .success(function(resp){profile_success(resp);})
+        .error(function(resp){profile_error(resp);});
+}
 
 function profile_success(resp) {
     avatar.addClass('custom_avatar');
