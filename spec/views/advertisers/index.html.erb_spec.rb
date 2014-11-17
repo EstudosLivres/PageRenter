a25c2b727cf7d0634069d0a7bc264f29bf69b656
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-17 14:48:52 -0200

    erb renamed to slim REFACTORED

--- spec/views/advertisers/index.html.erb_spec.rb
+++ spec/views/advertisers/index.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe "advertisers/index.html.erb" do
+describe "advertisers/index.html.slim" do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/publishers/index.html.erb_spec.rb
+++ spec/views/publishers/index.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 # TODO nos testes de views lembrar de testar se a funcionalidade para ela está presente e se tem todos conteúdos propostos
-describe "publishers/index.html.erb" do
+describe "publishers/index.html.slim" do
   pending "add some examples to (or delete) #{__FILE__}"
 end
