78d040ebcdacfaae31080802529a64f3093218f4
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-18 03:57:51 -0200

    bid with validations & commented

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,22 @@
 class Bid < ActiveRecord::Base
+  # Relations
   belongs_to :ad
   belongs_to :currency
+
+  # Custom validations
+
+  # Rails validations TODO: validate the min paid
+  validates :per_visitation, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
+  validates :per_foreign_interaction, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
+  validates :per_local_interaction, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
+  validates :per_conversion, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
+
+  # Validates Associations
+  validates :ad_id, presence: true, on: [:create, :update]
+  validates :currency_id, presence: true, on: [:create, :update]
+
+  # Return currency full name if there is a currency associated
+  def it_currency
+    currency.nil? ? nil : currency.acronym_with_name
+  end
 end
