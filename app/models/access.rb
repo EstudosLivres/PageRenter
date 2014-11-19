9069f027f207e183aba99fc80b429cef9e9d6ae5
Ilton Garcia <ilton_unb@hotmail.com>
2014-11-19 14:35:34 -0200

    base validations & method added

--- app/models/access.rb
+++ app/models/access.rb
@@ -1,4 +1,22 @@
 class Access < ActiveRecord::Base
+  # Relations
   belongs_to :profile
   belongs_to :ad
+
+  # Custom validations/setup
+  before_validation :setup
+
+  # Rails validations
+  validates :converted, presence: true, on: [:create, :update]
+  validates :recurrent, presence: true, on: [:create, :update]
+
+  # Validates Associations
+  validates :profile_id, presence: true, on: [:create, :update]
+  validates :ad_id, presence: true, on: [:create, :update]
+
+  private
+    def setup
+      self.converted = false if self.converted.nil?
+      self.recurrent = false if self.recurrent.nil?
+    end
 end
