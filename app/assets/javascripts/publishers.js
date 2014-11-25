fa4cdf5c60157033cabdfee916b05c755fabea7b
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-11-25 16:57:24 -0200

    ZeroClip (to add something to Control+C) moved from the HTML body into a JS file.

--- app/assets/javascripts/publishers.js
+++ app/assets/javascripts/publishers.js
@@ -1,3 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+// ZeroClip to copy to clipboard added (control+c)
+$(document).ready(function () {
+    // Create the object to be copied by the user
+    var clip = new ZeroClipboard($("#pagerenter_campaign"));
+});
