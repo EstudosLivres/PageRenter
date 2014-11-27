0423d4dd58097d0c9575c20c7da68fe68c3d67fc
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-11-27 15:12:22 -0200

    amount formatter to the operator format working & tested

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,5 @@
 class Currency < ActiveRecord::Base
+  # Associations
   has_many :bids
   has_many :financial_transactions
 
@@ -6,5 +7,24 @@ class Currency < ActiveRecord::Base
   def acronym_with_name
     name = I18n.t("db_enum.currencies.#{self.name}")
     "#{self.acronym} - #{name}"
+  end
+
+  # Receive it amount in any mask formatted (like: 1.000,00 or 1,000.00) Return the amount as operator need to be in string
+  def self.to_operator_str amount
+    # aux var to be more readable
+    decimal_cents = 2
+    unit_cents = 1
+    amount_length_index = amount.length
+    decimal_cents_index = amount_length_index-decimal_cents
+    unit_cents_index = amount_length_index-unit_cents
+
+    # aux vars to manipulate the return result
+    amount_cents = amount[decimal_cents_index, unit_cents_index]
+    raise   unless amount_cents.first.is_integer? || amount_cents.last.is_integer?
+    amount_without_cents = amount[0, decimal_cents_index]
+    amount_striped = amount_without_cents.remove('.').remove(',')
+
+    # Build the value as the operator understand, and return it
+    "#{amount_striped}#{amount_cents}"
   end
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Currency do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Operator Format value" do
+    # setup it vars to have the conversion tested, value: 1999 (nineteen ninety nine)
+    pt_br = '1.999,00'
+    en_us = '1,999.00'
+    value_expected = '199900'
+
+    # Convert PT_BR
+    it 'PT_BR should be convertible' do
+      pt_br_converted = Currency.to_operator_str pt_br
+
+      expect(pt_br_converted).to be_a String
+      expect(pt_br_converted.index('.')).to be_nil
+      expect(pt_br_converted.index('.,')).to be_nil
+      expect(pt_br_converted).to be_equals value_expected
+    end
+
+    # Convert EN_US
+    it 'EN_US should be convertible' do
+      en_us_converted = Currency.to_operator_str en_us
+
+      expect(en_us_converted).to be_a String
+      expect(en_us_converted.index('.')).to be_nil
+      expect(en_us_converted.index('.,')).to be_nil
+      expect(en_us_converted).to be_equals value_expected
+    end
+  end
 end
