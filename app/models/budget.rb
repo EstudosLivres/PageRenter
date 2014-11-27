9173020b8febd2dc99109afa6b68c584207aba65
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-11-27 15:25:21 -0200

    - Setup attrs before any validation & using specific formats; - Disable last budget preventing update if any error/raising occurred;

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -5,26 +5,37 @@ class Budget < ActiveRecord::Base
   # TODO has_many financial_transaction trough BudgetLaunches
 
   # Custom validations
-  validate :disable_last_budget, on: [:create] # TODO verify how does it work
+  before_validation :correct_format_for_values
+  before_validation :disable_last_budget, on: [:create]
 
   # Rails validations
-  validates :value, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 1, :less_than => 100000000}, on: [:create, :update]
   validates :closed_date, presence: true, on: :update
+  # The 100 means 1,00 or 1.00 on the Operator, it means 1 currency unity
+  validates :value, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 100, :less_than => 1000000000}, on: [:create, :update]
 
   # Validates Associations
   validates :currency_id, presence: true, on: [:create]
   validates :campaign_id, presence: true, on: [:create]
   validates :recurrence_period_id, presence: true, on: [:create]
 
-  # The budget have a history, so can't be UPDATED neither be DESTROYED
-  def disable_last_budget
-    last_budget = self.campaign.budgets.last
-    unless last_budget.nil?
-      last_budget.update(activated:false, closed_date:"#{Time.now()}")
-      # TODO prevent to persist if the last budget couldn't be UPDATED
+  # =============================== Private methods for callbakcs ============================
+  private
+    # SetUp it attrs to it correct values_format
+    def correct_format_for_values
+      self.value = Currency.to_operator_str self.value
     end
-  end
 
+    # The budget have a history, so can't be UPDATED neither be DESTROYED
+    def disable_last_budget
+      last_budget = self.campaign.budgets.last
+      unless last_budget.nil?
+        begin
+          last_budget.update(activated:false, closed_date:"#{Time.now()}")
+        rescue => e
+          self.errors.messages.add(e.message)
+        end
+      end
+    end
   # TODO activated significa que é o orçamento corrente este que o usuário está usando
   # TODO lembrar que os orçamentos nunca são editados, sempre são criados novos
 end
