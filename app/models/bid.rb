8880b2ea5940ace62c9618b0ac2202c5c2f18182
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-01 17:00:13 -0200

    Validations REFACTORED: - local interactions (likes, comments...): 1 cent; - foreign interactions (shares, retweets...): 2 cents; - visitation interactions: 5 cents; - conversion: 1 currency unit (Real, Dollar...)

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -6,10 +6,10 @@ class Bid < ActiveRecord::Base
   # Custom validations
 
   # Rails validations TODO: validate the min paid
-  validates :per_visitation, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
-  validates :per_foreign_interaction, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
-  validates :per_local_interaction, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
-  validates :per_conversion, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}, on: [:create, :update]
+  validates :per_local_interaction, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0.01}, on: [:create, :update]
+  validates :per_foreign_interaction, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0.02}, on: [:create, :update]
+  validates :per_visitation, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0.05}, on: [:create, :update]
+  validates :per_conversion, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 1.00}, on: [:create, :update]
 
   # Validates Associations
   validates :ad_id, presence: true, on: [:create, :update]
