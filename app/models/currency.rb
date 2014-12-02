7809c7547704f481295679651d7d0e68a9107799
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-02 07:47:30 -0200

    Converting the Decimal to the CardOperator pattern to process

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -9,22 +9,16 @@ class Currency < ActiveRecord::Base
     "#{self.acronym} - #{name}"
   end
 
-  # Receive it amount in any mask formatted (like: 1.000,00 or 1,000.00) Return the amount as operator need to be in string
+  # Receive it amount in Decimal and convert to operator str, like 10,00 to 1000
+  # * To use it remember to remove '.' and ',' from the controller params entries
   def self.to_operator_str amount
-    # aux var to be more readable
-    decimal_cents = 2
-    unit_cents = 1
-    amount_length_index = amount.length
-    decimal_cents_index = amount_length_index-decimal_cents
-    unit_cents_index = amount_length_index-unit_cents
+    # Check invalid entries
+    return nil if amount.nil?
 
-    # aux vars to manipulate the return result
-    amount_cents = amount[decimal_cents_index, unit_cents_index]
-    raise   unless amount_cents.first.is_integer? || amount_cents.last.is_integer?
-    amount_without_cents = amount[0, decimal_cents_index]
-    amount_striped = amount_without_cents.remove('.').remove(',')
-
-    # Build the value as the operator understand, and return it
-    "#{amount_striped}#{amount_cents}"
+    # Logical for DecimalAmount
+    amount_str = amount.to_s
+    the_dot_char = 1
+    dot_index = amount_str.index('.')
+    return amount_str[0..dot_index-the_dot_char]
   end
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -5,25 +5,31 @@ describe Currency do
     # setup it vars to have the conversion tested, value: 1999 (nineteen ninety nine)
     pt_br = '1.999,00'
     en_us = '1,999.00'
+
+    # it is the Rails conversion form the Form for Decimal input
+    decimal_obj_pt_br = BigDecimal.new(pt_br.remove('.').remove(','))
+    decimal_obj_en_us = BigDecimal.new(en_us.remove('.').remove(','))
+
+    # Value like the card operator uses
     value_expected = '199900'
 
     # Convert PT_BR
     it 'PT_BR should be convertible' do
-      pt_br_converted = Currency.to_operator_str pt_br
+      pt_br_converted = Currency.to_operator_str decimal_obj_pt_br
 
       expect(pt_br_converted).to be_a String
       expect(pt_br_converted.index('.')).to be_nil
-      expect(pt_br_converted.index('.,')).to be_nil
+      expect(pt_br_converted.index(',')).to be_nil
       expect(pt_br_converted).to be_equals value_expected
     end
 
     # Convert EN_US
     it 'EN_US should be convertible' do
-      en_us_converted = Currency.to_operator_str en_us
+      en_us_converted = Currency.to_operator_str decimal_obj_en_us
 
       expect(en_us_converted).to be_a String
       expect(en_us_converted.index('.')).to be_nil
-      expect(en_us_converted.index('.,')).to be_nil
+      expect(en_us_converted.index(',')).to be_nil
       expect(en_us_converted).to be_equals value_expected
     end
   end
