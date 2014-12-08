ba53097fc5a55f45e10b78b91629501897fca11d
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-08 18:05:11 -0200

    budget values solve (TODO: see if the bid is working)

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -13,8 +13,8 @@ class Budget < ActiveRecord::Base
 
   # Rails validations
   validates :closed_date, presence: true, on: :update
-  # The 100 means 1,00 or 1.00 on the Operator, it means 1 currency unity
-  validates :value, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 10, less_than: 100000}, on: [:create, :update]
+  # The 100 means 1,00 or 1.00 on the Operator, it means 1 currency unity (it is mult 100 because the 100 are the cents, so 10*100 is just 10 R$/US$...)
+  validates :value, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 50*100, less_than: 100000*100}, on: [:create, :update]
 
   # Validates Associations
   validates :currency_id, presence: true, on: [:create, :update]
@@ -28,15 +28,23 @@ class Budget < ActiveRecord::Base
     if transaction.nil? then FinancialTransaction.new else transaction end
   end
 
+  # Method to print the value on the masked form
   def operator_value_str
-    Currency.to_operator_str self.value
+    return self.value if Rents::Currency.have_cents? self.value if self.value
+    self.value*10 # it is to convert the int float, like 1.0 to 1.00
+  end
+
+  # ActiveRecord Get Method overwrite to print the real value, not the operator value
+  def value
+    # it is the real value, like 100, means R$1,00
+    read_attribute(:value).to_f/100 if read_attribute(:value)
   end
 
   # =============================== Private methods for callbakcs ============================
   private
     # SetUp it attrs to it correct values_format
     def correct_format_for_values
-      self.value = Currency.to_decimal self.value
+      self.value.to_i
     end
 
     # The budget have a history, so can't be UPDATED neither be DESTROYED
@@ -44,7 +52,7 @@ class Budget < ActiveRecord::Base
       last_budget = self.campaign.budgets.last
       unless last_budget.nil?
         begin
-          last_budget.update(activated:false, closed_date:"#{Time.now()}")
+          last_budget.update(active:false, closed_date:"#{Time.now()}")
         rescue => e
           self.errors.messages.add(e.message)
         end
@@ -56,6 +64,6 @@ class Budget < ActiveRecord::Base
 
     end
 
-  # TODO activated significa que é o orçamento corrente este que o usuário está usando
+  # TODO active significa que é o orçamento corrente este que o usuário está usando
   # TODO lembrar que os orçamentos nunca são editados, sempre são criados novos
 end
