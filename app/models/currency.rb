128ff5a11512c15319f982a6ace01f2da2deed8c
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-08 15:10:38 -0200

    Currency methods to conversions moved from it to the GEM

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -8,31 +8,4 @@ class Currency < ActiveRecord::Base
     name = I18n.t("db_enum.currencies.#{self.name}")
     "#{self.acronym} - #{name}"
   end
-
-  # Receive it amount in Decimal and convert to operator str, like 10,00 to 1000
-  # * To use it remember to remove '.' and ',' from the controller params entries
-  def self.to_operator_str amount
-    # Check invalid entries
-    return nil if amount.nil?
-
-    # Logical for DecimalAmount
-    amount_str = amount.to_s
-    the_dot_char = 1
-    dot_index = amount_str.index('.')
-    return amount_str[0..dot_index-the_dot_char]
-  end
-
-  # Receive it amount in Integer String and convert to Decimal value
-  def self.decimal_from_operator_str amount
-    # Base vars
-    aux_amount = amount
-    cents_chars_counter = 2
-
-    # Building the currency str like BigDecimal understands
-    cents_str = aux_amount[aux_amount.length-cents_chars_counter..aux_amount.length]
-    currency_integer_str = aux_amount[0, aux_amount.length-cents_chars_counter]
-    new_amount = "#{currency_integer_str}.#{cents_str}"
-
-    BigDecimal.new new_amount
-  end
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,47 +1,4 @@
 require 'spec_helper'
 
 describe Currency do
-  context "Operator Format value" do
-    # setup it vars to have the conversion tested, value: 1999 (nineteen ninety nine)
-    pt_br = '1.999,00'
-    en_us = '1,999.00'
-
-    # Value like the card operator uses
-    operator_expected_value = '199900'
-    decimal_expected_value = BigDecimal.new '1999.00'
-
-    # it is the Rails conversion form the Form for Decimal input
-    decimal_obj_pt_br = BigDecimal.new(pt_br.remove('.').remove(','))
-    decimal_obj_en_us = BigDecimal.new(en_us.remove('.').remove(','))
-
-    # Conversions
-    pt_br_converted = Currency.to_operator_str decimal_obj_pt_br
-    en_us_converted = Currency.to_operator_str decimal_obj_en_us
-    decimal_converted = Currency.decimal_from_operator_str operator_expected_value
-
-    context 'Decimal to Operator' do
-      # Convert PT_BR
-      it 'PT_BR should be convertible' do
-        expect(pt_br_converted).to be_a String
-        expect(pt_br_converted.index('.')).to be_nil
-        expect(pt_br_converted.index(',')).to be_nil
-        expect(pt_br_converted).to be_equals operator_expected_value
-      end
-
-      # Convert EN_US
-      it 'EN_US should be convertible' do
-        expect(en_us_converted).to be_a String
-        expect(en_us_converted.index('.')).to be_nil
-        expect(en_us_converted.index(',')).to be_nil
-        expect(en_us_converted).to be_equals operator_expected_value
-      end
-    end
-
-    context 'Operator to Decimal' do
-      it 'must be simple decimal, not formatted' do
-        expect(decimal_converted).to be_a BigDecimal
-        expect(decimal_converted).to be == decimal_expected_value
-      end
-    end
-  end
 end
