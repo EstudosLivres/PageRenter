b2505ca90909e5d5306789a007247526a47972f0
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-09 16:26:40 -0200

    paid? method REFACTORED

--- app/models/financial_transaction.rb
+++ app/models/financial_transaction.rb
@@ -3,7 +3,6 @@ class FinancialTransaction < ActiveRecord::Base
   has_one :receipt
   belongs_to :budget
   belongs_to :currency
-  belongs_to :payment_method
   belongs_to :payer, :class_name => 'Campaign', foreign_key: :payer_id
   belongs_to :receiver, :class_name => 'User', foreign_key: :receiver_id
 
@@ -12,19 +11,33 @@ class FinancialTransaction < ActiveRecord::Base
 
   # Validates Attrs
   validates :value, length: { in: 2..10 }, presence: true, on: [:create, :update] # 2 chars means it cents
-  validates :withdraw, presence: true, on: [:create, :update] # means if it is Page earn or User earn
+  validates_inclusion_of :withdraw, in: [true, false], on: [:create, :update] # means if it is Page earn or User earn
   validates :payment_method, length: { in: 5..50 }, presence: true, on: [:create, :update]
   validates :remote_id, presence: true, on: [:update]
 
   # Validates Associations
-  validates :user_id, presence: true, on: [:create, :update]
   validates :currency_id, presence: true, on: [:create, :update]
 
   # TODO enum banking_or_online (TRUE is banking & FALSE if online/system)
 
   # Return true if it is paid & false if it is not paid yet
   def paid?
-    self.remote_id.nil? ? false : true
+    initial_status = 0
+    return false if self.status_code == initial_status
+    self.status_name == 'error' ? true : false if self.status_code != initial_status
+    self.status_name == 'charged' ? true : false if self.status_code != initial_status
+
+    # Verifying transaction
+    transaction = Rents::Transaction.new(rid:remote_id)
+    transaction_verified = transaction.verify
+
+    # Update it attrs
+    self.status_code=transaction_verified[:status][:code]
+    self.status_name=transaction_verified[:status][:name]
+
+    # Prepare it returning
+    self.status_name == 'error' ? true : false if self.status_code != initial_status
+    self.status_name == 'charged' ? true : false if self.status_code != initial_status
   end
 
   private
