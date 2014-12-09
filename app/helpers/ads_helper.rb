98ac19b75d47e2503f0174d53006225fb1c04027
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-09 09:22:16 -0200

    helper to create the shorter Ad URL to be shared

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,2 +1,13 @@
 module AdsHelper
+  def shorter_url domain, publisher_username, ad_username
+    # SetUp the base vars
+    shorter_api_url = "https://www.googleapis.com/urlshortener/v1/url"
+    path = "#{publisher_username}/#{ad_username}"
+    domain = domain+'/' if domain.last != '/'
+    domain = "#{domain}accesses/" if domain.index('accesses').nil?
+
+    # Build the link to be shortened & shorter it to return it shortened
+    url = "#{domain}#{path}"
+    RestClient.post shorter_api_url, {'longUrl'=>url}.to_json, content_type: :json
+  end
 end
