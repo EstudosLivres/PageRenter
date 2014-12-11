20374627c4b247b0b1851d4d6181d502516ec621
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-11 18:41:53 -0200

    Base URL shorter code added

--- lib/google/shorter.rb
+++ lib/google/shorter.rb
@@ -1,8 +1,10 @@
-require 'google/google'
 class Google::Shorter
+  # URL for the Google Shorter API
+  @@shorter_api_url = "https://www.googleapis.com/urlshortener/v1/url"
+
   # Generate it shorter URL based ont he LongURL passed
   def self.generate url
-    resp = RestClient.post Google.shorter_api_url, {'longUrl'=>url}.to_json, content_type: :json
-    JSON.parse(resp).symbolize_keys
+    resp = RestClient.post @@shorter_api_url, {'longUrl'=>url}.to_json, content_type: :json
+    JSON.parse(resp).symbolize_keys()[:id]
   end
 end
