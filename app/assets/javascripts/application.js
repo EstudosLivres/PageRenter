9083d04255c79e20db0e01491f7fb89d4b026ea3
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-11 15:03:49 -0200

    jquery shorter removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,9 +21,5 @@
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.pt-BR.js
 //= require zeroclipboard
 //= require mask_money
-//= require jquery.urlshortener.min
 //= require app
 //= require sign
-
-// SetUp the sortener API_KEY
-jQuery.urlShortener.settings.apiKey = 'AIzaSyCJOefXdhTtFIX9Y-G2IAFNJrAlZ2PkQhg';
