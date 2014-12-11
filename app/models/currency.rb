07edfad92ffcc9cd534c087862c34bd30f35f18f
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-11 09:30:24 -0200

    safe patter added (if is necessary to call it acronym & there is no currency instance (is an empty instance)

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -6,6 +6,12 @@ class Currency < ActiveRecord::Base
   # Aux methods to return better understandable
   def acronym_with_name
     name = I18n.t("db_enum.currencies.#{self.name}")
+    return '' if self.acronym.nil? || self.name.nil?
     "#{self.acronym} - #{name}"
+  end
+
+  # DesignPattern, if there is a place where it must be print & is possible to have no relation, call the safe
+  def safe_acronym
+    self.acronym.nil? ? '' : self.acronym
   end
 end
