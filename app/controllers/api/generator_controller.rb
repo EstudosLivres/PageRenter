7e08fb12a836126ed18379663af07cc31459d259
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-12 09:30:19 -0200

    The returning messages in GET HTTP PARAM

--- app/controllers/api/generator_controller.rb
+++ app/controllers/api/generator_controller.rb
@@ -8,8 +8,8 @@ class API::GeneratorController < API::BaseAPIController
     shorter.errors.messages.delete(:link) unless shorter.errors.messages[:link].first.index('taken').nil? unless shorter.errors.empty?
 
     # Abort if Publisher or Ad not found (prevent to consume Shorter)
-    success_msg = 'A URL requisitada foi encurtada com sucesso!'
-    error_msg = 'Ocorreu algum erro ao tentar encurtar a URL requisitada!'
+    success_msg = 'A URL requisitada foi encurtada com sucesso!'.to_get_param
+    error_msg = 'Ocorreu algum erro ao tentar encurtar a URL requisitada!'.to_get_param
     shorter.errors.empty? ? redirect_to("#{publisher_root_url}?success=#{success_msg}") : redirect_to("#{publisher_root_url}?error=#{error_msg}")
   end
 end
