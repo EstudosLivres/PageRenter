4b610f33a1701d93084652daf059e5181d347187
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-12 11:27:36 -0200

    all access registration moved from it controller to the access controller

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,7 +1,4 @@
 class PublishersController < ApplicationController
-  # Custom layouts for specific actions
-  layout 'blank', only: [:brought_access]
-
   # Prevent spam USER.FIND on DB
   before_action :set_publisher, except: [:brought_access]
 
@@ -36,18 +33,6 @@ class PublishersController < ApplicationController
     end
   end
 
-  # GET '/accesses/:publisher_username/:ad_username' -> register it access brought through it publisher
-  def brought_access
-    begin
-      # Register it access
-      Access.new(ad_id:ad,profile_id:publisher)
-    rescue => e
-      render file: "#{Rails.root}/public/404_accesses"
-      puts "MethodError on: #{__method__}. \nError: "
-      puts e
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_publisher
@@ -57,15 +42,5 @@ class PublishersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def publisher_params
       params.require(:publisher).permit(:name)
-    end
-
-    # SetUp all the validations to guarantee it uniqueness
-    def register_access_to_be_unique
-      # :acid = access_ids (expression to register it access)
-      session[:acid] = [] if session[:acid].nil?
-      # That expression is compressed because the session isn't so big to contain a lot of characters
-      aid_expression = "#{params[:publisher_id]}-#{params[:ad_id]}"
-      # Using unshift (push in the begging of the array because if the user is recurrent it is more probably to be the last access)
-      session[:acid].unshift aid_expression
     end
 end
