72ae88ac9f8d680e9ffb79cdea8f3c29d9c0b03e
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-12 08:37:55 -0200

    APIKey added

--- lib/google/shorter.rb
+++ lib/google/shorter.rb
@@ -1,10 +1,12 @@
 class Google::Shorter
   # URL for the Google Shorter API
+  @@shorter_api_key = '?key=AIzaSyCJOefXdhTtFIX9Y-G2IAFNJrAlZ2PkQhg'
   @@shorter_api_url = "https://www.googleapis.com/urlshortener/v1/url"
 
   # Generate it shorter URL based ont he LongURL passed
   def self.generate url
-    resp = RestClient.post @@shorter_api_url, {'longUrl'=>url}.to_json, content_type: :json
+    api_url = @@shorter_api_url + @@shorter_api_key
+    resp = RestClient.post api_url, {'longUrl'=>url}.to_json, content_type: :json
     JSON.parse(resp).symbolize_keys()[:id]
   end
 end
