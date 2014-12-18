e902b914d845906663c6d1c02d9e9f75dd7833f7
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-18 11:01:58 -0200

    fonts folder added to the assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,9 @@ module PageRenter
       Devise::SessionsController.layout proc{ |controller| action_name == 'new' ? "sign"   : "application" }
     end
 
+    # Adding the fonts
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.default_locale = :'pt-BR'
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
