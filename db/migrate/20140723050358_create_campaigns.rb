62cf6355e7dd1c02429aa50354a244c3915fbea4
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-22 16:49:46 -0200

    campaign type association added

--- db/migrate/20140723050358_create_campaigns.rb
+++ db/migrate/20140723050358_create_campaigns.rb
@@ -1,10 +1,16 @@
 class CreateCampaigns < ActiveRecord::Migration
   def change
     create_table :campaigns do |t|
+      # Identifier for the user
       t.string :name, limit: 75, null: false
+
+      # The date references
       t.datetime :launch_date, null: true
       t.datetime :end_date, null: true
+
+      # Associations
       t.references :advertiser, index: true
+      t.belongs_to :campaign_type
 
       t.timestamps
     end
