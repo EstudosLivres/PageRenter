6b2da2b9a08f4c6c343381ce463c30fbc7606e1a
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-22 16:51:45 -0200

    setting it campaign types added

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,5 @@
 class CampaignsController < ApplicationController
+  before_action :setup_aux_objs
   before_action :set_campaign, only: [:show, :edit, :update, :destroy]
 
   # GET /campaigns
@@ -74,5 +75,10 @@ class CampaignsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def campaign_params
       params.require(:campaign).permit(:name, :launch_date, :end_date)
+    end
+
+    # Create objs to auxiliary on create forms
+    def setup_aux_objs
+      @campaign_types = CampaignType.all
     end
 end
