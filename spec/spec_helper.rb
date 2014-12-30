757146b8b53d508d9d56b50a0bb642d392331cf2
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-30 21:44:07 -0200

    factory girl moved to another line & devise helper added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,9 +20,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-  # Config the RSPec Plugin
-  config.include FactoryGirl::Syntax::Methods
-
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -30,6 +27,12 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+
+  # Config the RSPec Plugin
+  config.include FactoryGirl::Syntax::Methods
+
+  # Adding devise
+  config.include Devise::TestHelpers, type: :controller
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
