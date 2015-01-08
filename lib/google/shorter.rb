079dcb73dae2792fc328e5cd9d997be50fda1891
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-08 16:17:36 -0200

    consts for Google module added on the shorter file

--- lib/google/shorter.rb
+++ lib/google/shorter.rb
@@ -1,6 +1,15 @@
-require 'google/google'
-
 module Google
+  # URL for the Google Shorter API
+  @google_credentials = Rails.application.secrets.google.symbolize_keys
+  #@google_credentials_web = @google_credentials[:web]
+  #@google_credentials_server = @google_credentials[:server]
+  @shorter_api_key = "?key=#{@google_credentials[:global_key]}"
+  @shorter_api_url = "https://www.googleapis.com/urlshortener/v1/url"
+
+  def self.shorter_url
+    @shorter_api_url + @shorter_api_key
+  end
+
   class Shorter
     # Generate it shorter URL based ont he LongURL passed
     def self.generate url
